b261acb3fea25ca33f70c03738abb53f03e0c846
Rodri10s <ramonrodrigues838@gmail.com>
2025-04-29 21:16:40 -0300

    inserção do javascript

---
+++ 4 - POSITION ANIMATION/js/scripts.js
@@ -0,0 +1,9 @@
+function controle(){
+    var but = document.getElementById("btn");
+    var b1 = document.getElementById("bola-01");
+    var box = document.getElementById("container");
+
+    if (but.innerText == "PAUSE"){
+        box.style.backgroundColor = "#000"
+    }
+}
