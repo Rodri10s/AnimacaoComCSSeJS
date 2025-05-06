63f37e936296c4c880435da73d85612ee736c6dc
Rodri10s <ramonrodrigues838@gmail.com>
2025-05-05 22:19:53 -0300

    pause funcional

--- 4 - POSITION ANIMATION/js/scripts.js
+++ 4 - POSITION ANIMATION/js/scripts.js
@@ -1,9 +1,24 @@
 function controle(){
     var but = document.getElementById("btn");
     var b1 = document.getElementById("bola-01");
+    var b2 = document.getElementById("bola-02");
+    var b3 = document.getElementById("bola-03");
+    var b4 = document.getElementById("bola-04");
     var box = document.getElementById("container");
 
     if (but.innerText == "PAUSE"){
         box.style.backgroundColor = "#000"
+        btn.innerText = "PLAY";
+        b1.style.animationPlayState = "paused";
+        b2.style.animationPlayState = "paused";
+        b3.style.animationPlayState = "paused";
+        b4.style.animationPlayState = "paused";
+    } else {
+        box.style.backgroundColor = "#FFF"
+        btn.innerText = "PAUSE";
+        b1.style.animationPlayState = "running";
+        b2.style.animationPlayState = "running";
+        b3.style.animationPlayState = "running";
+        b4.style.animationPlayState = "running";
     }
 }
